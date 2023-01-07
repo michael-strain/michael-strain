475433df8c8395590fab967f157c6425fc419aa2
ashley-strain <89878817+ashley-strain@users.noreply.github.com>
2023-01-07 01:20:18 +0000

    Working wrap.

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -3,9 +3,19 @@
 <template>
   <div>
     <v-container>
+      <h3 class="justify-center text-center align-center font-bold text-5xl mb-10">PAGE NAME</h3>
+      
+      <!--Quick Links-->
+      <ul class="flex flex-wrap justify-center text-center align-center text-lg m-10">
+        <li>Puzzles</li>
+        <li>Cups</li>
+        <li>Towels</li>
+      </ul>
+
+
       <div class="flex flex-wrap items-center align-center justify-center">
         <!--Beginfirstcard-->
-        <v-card class="m-5 flex-grow max-w-sm sm:max-w-xs">
+        <v-card class="flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             class="w-full"
@@ -39,7 +49,7 @@
         <!-- End first card-->
 
         <!-- Begin Second card-->
-        <v-card class="m-5 flex-grow max-w-sm sm:max-w-xs">
+        <v-card class="flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             class="w-full"
@@ -73,7 +83,8 @@
         <!--End second card-->
 
         <!-- Begin third card -->
-        <v-card class="m-5 flex-grow max-w-sm sm:max-w-xs">
+        <!--<v-card class="flex-grow m-5 max-w-sm sm:max-w-xs"></v-card>-->
+        <v-card class="flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             class="w-full"
@@ -109,128 +120,6 @@
     </v-container>
   </div>
 </template>
-
-
-<!--
-<template>
-  <div>
-    <v-container>
-      <div class="flex flex-wrap justify-space-between">
-        <v-card class="mx-auto flex m-3">
-          <img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            class="h-full m-0 p-0 contain"
-          >
-
-          <v-card-title class="text-h5 justify-center text-center align-center">
-            Top western road trips
-          </v-card-title>
-
-          <div class="d-flex items-center justify-center text-center align-center m-0 pt-5">
-            <v-card-actions>
-              <v-btn
-                icon
-                @click="leftArrow"
-              >
-                <v-icon icon="mdi-chevron-left" />
-              </v-btn>
-            
-              <v-btn
-                icon
-                @click="show = !show; heartClick()"
-              >
-                <v-icon>{{ show ? 'mdi-cards-heart-outline' : 'mdi-cards-heart' }}</v-icon>
-              </v-btn>
-
-              <v-btn
-                icon
-                @click="rightArrow"
-              >
-                <v-icon icon="mdi-chevron-right" />
-              </v-btn>
-            </v-card-actions>
-          </div>
-
-          <v-expand-transition />
-        </v-card>
-        <v-card class="mx-auto flex m-3">
-          <img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            class="h-full m-0 p-0 contain"
-          >
-
-          <v-card-title class="justify-center text-center align-center">
-            Top western road trips
-          </v-card-title>
-
-          <div class="d-flex items-center justify-center text-center align-center m-0 pt-5">
-            <v-card-actions>
-              <v-btn
-                icon
-                @click="leftArrow"
-              >
-                <v-icon icon="mdi-chevron-left" />
-              </v-btn>
-            
-              <v-btn
-                icon
-                @click="show = !show; heartClick()"
-              >
-                <v-icon>{{ show ? 'mdi-cards-heart-outline' : 'mdi-cards-heart' }}</v-icon>
-              </v-btn>
-
-              <v-btn
-                icon
-                @click="rightArrow"
-              >
-                <v-icon icon="mdi-chevron-right" />
-              </v-btn>
-            </v-card-actions>
-          </div>
-
-          <v-expand-transition />
-        </v-card>
-        <v-card class="mx-auto flex m-3">
-          <img
-            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            class="h-full m-0 p-0 contain"
-          >
-
-          <v-card-title class="justify-center text-center align-center">
-            Top western road trips
-          </v-card-title>
-          <div class="d-flex items-center justify-center text-center align-center m-0 pt-5">
-            <v-card-actions>
-              <v-btn
-                icon
-                @click="leftArrow"
-              >
-                <v-icon icon="mdi-chevron-left" />
-              </v-btn>
-            
-              <v-btn
-                icon
-                @click="show = !show; heartClick()"
-              >
-                <v-icon>{{ show ? 'mdi-cards-heart-outline' : 'mdi-cards-heart' }}</v-icon>
-              </v-btn>
-
-              <v-btn
-                icon
-                @click="rightArrow"
-              >
-                <v-icon icon="mdi-chevron-right" />
-              </v-btn>
-            </v-card-actions>
-          </div>
-
-          <v-expand-transition />
-        </v-card>
-
-      </div>
-    </v-container>
-  </div>
-</template> -->
 
 
 <script setup>
