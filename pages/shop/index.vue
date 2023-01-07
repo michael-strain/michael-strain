aa8a8fda3d0710cfcf87cee96d5c74768c0e7955
ashley-strain <89878817+ashley-strain@users.noreply.github.com>
2023-01-07 04:06:46 +0000

    Shop Page Looks Good

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -3,27 +3,36 @@
 <template>
   <div>
     <v-container>
-      <h3 class="justify-center text-center align-center font-bold text-5xl mb-10">PAGE NAME</h3>
+      <h3 class="justify-center text-center align-center font-bold text-5xl mb-10" :style="{fontFamily: 'Roboto Slab', textShadow: '3px 3px rgba(52, 211, 153)'}">
+        SHOP
+      </h3>
       
       <!--Quick Links-->
-      <ul class="flex flex-wrap justify-center text-center align-center text-lg m-10">
-        <li>Puzzles</li>
-        <li>Cups</li>
-        <li>Towels</li>
+      <ul class="text-green-400 m-5 text-lg flex flex-wrap justify-center text-center align-center text-lg" :style="{fontFamily: 'Roboto Slab'}">
+        <li class="m-3 hover:text-green-600"><a href="http://localhost:3000/">PUZZLES</a></li>
+        <li class="m-3 hover:text-green-600"><a href="http://localhost:3000/">TOWELS</a></li>
+        <li class="m-3 hover:text-green-600"><a href="http://localhost:3000/">CUPS</a></li>
+        
       </ul>
 
+      <!-- <div class="bg-gray-100 rounded-xl shadow-md p-10 border">
+          <v-card-title class="justify-center text-center align-center">
+            <h5 class="font-bold text-4xl">Puzzles</h5>
+          </v-card-title>
+          <h3 class="justify-center text-center align-center">Glossy puzzles that come in four sizes: 110, 252, 500, and 1014 pieces.</h3>
+        </div> -->
 
       <div class="flex flex-wrap items-center align-center justify-center">
         <!--Beginfirstcard-->
-        <v-card class="flex-1 m-5 min-w-xs">
+        <v-card class="rounded-xl border shadow-md flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-            class="w-full"
+            class="w-fit"
           >
-          <v-card-title class="text-h5 justify-center text-center align-center">
-            Fuck 
+          <v-card-title class="bg-gray-100 text-h5 justify-center text-center align-center">
+            <p :style="{fontFamily: 'Roboto Slab'}">Fuck</p> 
           </v-card-title>
-          <div class="d-flex items-center justify-center text-center align-center m-0">
+          <div class="bg-gray-100 d-flex items-center justify-center text-center align-center m-0">
             <v-card-actions>
               <v-btn
                 icon
@@ -49,15 +58,15 @@
         <!-- End first card-->
 
         <!-- Begin Second card-->
-        <v-card class="flex-1 m-5 min-w-xs">
+        <v-card class="rounded-xl flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             class="w-full"
           >
-          <v-card-title class="text-h5 justify-center text-center align-center">
+          <v-card-title class="bg-gray-100 text-h5 justify-center text-center align-center">
             Fuck 
           </v-card-title>
-          <div class="d-flex items-center justify-center text-center align-center m-0">
+          <div class="bg-gray-100 d-flex items-center justify-center text-center align-center m-0">
             <v-card-actions>
               <v-btn
                 icon
@@ -83,16 +92,15 @@
         <!--End second card-->
 
         <!-- Begin third card -->
-        <!--<v-card class="flex-grow m-5 max-w-sm sm:max-w-xs"></v-card>-->
-        <v-card class="flex-1 m-5 min-w-xs">
+        <v-card class="rounded-xl flex-1 m-5 min-w-xs">
           <img
             src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
             class="w-full"
           >
-          <v-card-title class="text-h5 justify-center text-center align-center">
+          <v-card-title class="bg-gray-100 text-h5 justify-center text-center align-center">
             Fuck 
           </v-card-title>
-          <div class="d-flex items-center justify-center text-center align-center m-0">
+          <div class="bg-gray-100 d-flex items-center justify-center text-center align-center m-0">
             <v-card-actions>
               <v-btn
                 icon
