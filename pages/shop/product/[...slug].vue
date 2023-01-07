d0e15b873c4a3831d5ca519bc9a098136110eb6f
michael-strain <michael.v.strain@gmail.com>
2023-01-07 03:59:14 +0000

    connected the printify api

--- pages/shop/product/[...slug].vue
+++ pages/shop/product/[...slug].vue
@@ -12,6 +12,15 @@
 <script setup>
 import { ref } from 'vue'
 
+// Check this out for setting a nuxt api call to make unique id's using a counter
+// https://nuxt.com/docs/getting-started/data-fetching#example-2
+
+const productKeys = [
+  {'name':'Test-Puzzle','id':'63b73e95f65b2192630ec97a'},
+  {'name':'Test-Hand-Towel','id':'63b74271dbffaaf4fc0393ed'},
+  {'name':'Test-Color-Morphing-Mug','id':'63b7410a0b5a3e94ee0565ad'}
+]
+
 const shopId = '6483145'
 const baseUrl = `https://api.printify.com/v1/shops/${shopId}`
 const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6ImE2MGI5ZWEyYzRhODliM2VmYWIzNThhNWIyOTE3ZDc5MDNiYjM2NDdmZjIzYTM5NWM4YjM3OGViYzZjMWIwOTNlOTdiOGYxZGM3YWZhZTg3IiwiaWF0IjoxNjczMDUyOTAzLjQ3NTY0MiwibmJmIjoxNjczMDUyOTAzLjQ3NTY0NSwiZXhwIjoxNzA0NTg4OTAzLjQ0ODc0NCwic3ViIjoiMTEzMDIzOTkiLCJzY29wZXMiOlsic2hvcHMubWFuYWdlIiwic2hvcHMucmVhZCIsImNhdGFsb2cucmVhZCIsIm9yZGVycy5yZWFkIiwib3JkZXJzLndyaXRlIiwicHJvZHVjdHMucmVhZCIsInByb2R1Y3RzLndyaXRlIiwid2ViaG9va3MucmVhZCIsIndlYmhvb2tzLndyaXRlIiwidXBsb2Fkcy5yZWFkIiwidXBsb2Fkcy53cml0ZSIsInByaW50X3Byb3ZpZGVycy5yZWFkIl19.AH6QPYSJpX5z7YyO8dW5nTpS_CrorLN3gJDJ_k8v58waX1cBIkQCD5qTPE8hLLFFDr61lNgvUPpcCDXd0-Q'
@@ -28,7 +37,43 @@ const postCalcShipping = `/orders/shipping.json`
 //  Now we are going to specify required inputs for the queries
 //
 
-// POST Order Properties
+// POST Order Body
+const postOrderBody = 
+{
+    "external_id": "<string>",
+    "line_items": [
+        {
+            "product_id": "<string>",
+            "print_provider_id": "<integer>",
+            "blueprint_id": "<integer>",
+            "variant_id": "<integer>",
+            "print_areas": "<object>",
+            "quantity": "<integer>"
+        },
+        {
+            "product_id": "<string>",
+            "print_provider_id": "<integer>",
+            "blueprint_id": "<integer>",
+            "variant_id": "<integer>",
+            "print_areas": "<object>",
+            "quantity": "<integer>"
+        }
+    ],
+    "shipping_method": 1,
+    "send_shipping_notification": "<boolean>",
+    "address_to": {
+        "first_name": "<string>",
+        "last_name": "<string>",
+        "email": "<string>",
+        "phone": "<string>",
+        "country": "<string>",
+        "region": "<string>",
+        "address1": "<string>",
+        "address2": "<string>",
+        "city": "<string>",
+        "zip": "<string>"
+    }
+}
 
 
 </script>
