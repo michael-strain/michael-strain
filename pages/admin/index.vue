88434dbe7ec283821253b56c5c5d0e9aed38802a
michael-strain <michael.v.strain@gmail.com>
2023-01-19 01:06:10 +0000

    creating new pages, epanding todo list

---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Admin</h1>
+    <p>Admin page</p>
+    <p>Only logged in users can see this page</p>
+    <p>Well... that's the idea at least.  Functionality not yet enabled.</p>
+  </div>
+</template>
