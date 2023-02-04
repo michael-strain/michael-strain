3b3e2899bfae90b5d12e7ef5454f79bd3545d155
ashley-strain <ashley.n.strain@gmail.com>
2023-02-04 00:08:26 -0500

    Restaurant: Section 1 positioning. Thanks babe! :*

--- pages/restaurantex.vue
+++ pages/restaurantex.vue
@@ -4,15 +4,12 @@
     All asthetics.
   
   Section One: 
-    Center text div.
     Text spacing.
     Transparent div.
 
   Section Two:
 
   Section Three:
-  
-  
 
 -->
 <template>
@@ -20,63 +17,65 @@
   <div class="bg-gradient-to-b from-green-300 to-blue-300">
     <v-parallax
       src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-      class="flex text-center align-middle items-center justify-center h-screen w-screen"
+      class="flex text-center align-middle items-center h-screen w-full"
     >
-      <div class="w-4/5 justify-center ">
-        <div class="bg-white">
-          <h1 class="text-h3 !font-bold sm:text-h3">
-            Restaurant Example Header
-          </h1>
-          <p class="my-5 italic text-3xl">
-            Relate your restaurant's story to its core values. Tell them what struggles you had to overcome, what you learned from it, and how it impacted your values.
-          </p>
-          <div class=" items-center align-center justify-center text-2xl">
-            <div class="flex mx-5">
-              <v-icon
-                class="mr-3"
-                icon="mdi-candy-outline"
-              />
-              <p>Open Monday thru Saturday, 8AM - 10PM</p>
+      <div class="flex text-center align-middle items-center justify-center flex-shrink">
+        <div class="w-4/5 justify-center my-15">
+          <div class="bg-white mx-2 p-7">
+            <div class="">
+              <h1 class="text-h4 !font-bold sm:text-h3">
+                <!-- Old -->
+                <!-- <div class="w-4/5 justify-center ">
+                      <div class="bg-white">
+                        <h1 class="text-h3 !font-bold sm:text-h3"> -->
+                Restaurant Example Header
+              </h1>
+              <p class="my-5 italic text-3xl">
+                Relate your restaurant's story to its core values. Tell them what struggles you had to overcome, what you learned from it, and how it impacted your values.
+              </p>
+              <div class=" items-center align-center justify-center text-2xl">
+                <div class="flex mx-5">
+                  <v-icon
+                    class="mr-3"
+                    icon="mdi-candy-outline"
+                  />
+                  <p>Open Monday thru Saturday, 8AM - 10PM</p>
+                </div>
+                <div class="flex mx-5">
+                  <v-icon
+                    class="mr-3"
+                    icon="mdi-candy-outline"
+                  />
+                  <p>852 Maybell Street, Shitty City, State</p>
+                </div>
+                <div class="flex mx-5">
+                  <v-icon
+                    class="mr-3"
+                    icon="mdi-candy-outline"
+                  />
+                  <p>+507 - 963 - 1478</p>
+                </div>
+              </div>
             </div>
-            <div class="flex mx-5">
-              <v-icon
-                class="mr-3"
-                icon="mdi-candy-outline"
-              />
-              <p>852 Maybell Street, Shitty City, State</p>
-            </div>
-            <div class="flex mx-5">
-              <v-icon
-                class="mr-3"
-                icon="mdi-candy-outline"
-              />
-              <p>+507 - 963 - 1478</p>
-            </div>
+            <!-- <v-divider
+              vertical
+              class="p-5"
+            /> -->
+                  
+            <!--<img
+              src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" 
+              class="bg-red-100 object-scale-down"
+            > -->
+        
+            <!--Icon-->
+            <!-- <v-icon
+              size="300"
+              color="white"
+              icon="mdi-image-plus-outline"
+              class="bg-rose-700"
+            /> -->
           </div>
         </div>
-        <!-- <v-divider
-          vertical
-          class="p-5"
-        /> -->
-              
-        <!-- <v-col
-        cols=""
-        sm=""
-        class="justify-center items-center flex outline"
-      >
-        <img
-          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" 
-          class="bg-red-100 object-scale-down"
-        > -->
-    
-        <!--Icon-->
-        <!-- <v-icon
-          size="300"
-          color="white"
-          icon="mdi-image-plus-outline"
-          class="bg-rose-700"
-        />
-        </v-col> -->
       </div>
     </v-parallax>
   </div>
