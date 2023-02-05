6e1a6ce1b614d9a6c8c9314e53dd882be56876ab
ashley-strain <ashley.n.strain@gmail.com>
2023-02-05 09:08:41 -0500

    Test: Basic outline complete.

---
+++ pages/restaurant/test.vue
@@ -0,0 +1,95 @@
+<template>
+
+  <!--CAROUSEL-->
+
+  <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows="hover"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+
+  <!--TABS-->
+  <v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold text-h2 text-basil">
+        BASiL
+      </h1>
+    </v-card-title>
+
+    <v-tabs
+      v-model="tab"
+      bg-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item"
+        :value="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-window v-model="tab">
+      <v-window-item
+        v-for="item in items"
+        :key="item"
+        :value="item"
+      >
+        <v-card
+          color="basil"
+          flat
+        >
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-window-item>
+    </v-window>
+  </v-card>
+
+</template>
+<!--SCRIPT-->
+<script>
+  export default {
+    data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+        tab: 'Appetizers',
+        items: [
+          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+      }
+  }
+</script>
