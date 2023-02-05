dda10130ba4b8c4d1aa0f39413ef95d36b3c4e6d
ashley-strain <ashley.n.strain@gmail.com>
2023-02-05 09:42:37 -0500

    Test Page: Experimenting with cards layout.

--- pages/restaurant/test.vue
+++ pages/restaurant/test.vue
@@ -1,5 +1,18 @@
 <template>
+  <!--To Do:
+  
+  Section One:
+    Carosal background. Can this be done with a paralax as well?
+    Name of dish somewhere on the carosal image.
 
+  Section Two: 
+    If adding images for each dish, use hover > transations for layout.
+    Add icon to bottom right corner of card for special information.
+    Try using tabs for the different categories with grow prop.
+    Try replacing divs with cards.  
+    Fix that the grid system is not sliding like first card.
+
+-->
   <!--CAROUSEL-->
 
   <v-carousel
@@ -27,12 +40,14 @@
 
   <!--TABS-->
   <v-card color="basil">
+    <!--TABS TITLE-->
     <v-card-title class="text-center justify-center py-6">
       <h1 class="font-weight-bold text-h2 text-basil">
-        BASiL
+        MENU
       </h1>
     </v-card-title>
 
+    <!--TABS NAV BAR-->
     <v-tabs
       v-model="tab"
       bg-color="transparent"
@@ -47,24 +62,91 @@
         {{ item }}
       </v-tab>
     </v-tabs>
-
+    <!--HOVER-->
     <v-window v-model="tab">
       <v-window-item
         v-for="item in items"
         :key="item"
         :value="item"
       >
-        <v-card
-          color="basil"
-          flat
-        >
-          <v-card-text>{{ text }}</v-card-text>
-        </v-card>
+        <div>
+          <v-hover v-slot="{ isHovering, props }">
+            <v-card
+              class="mx-auto"
+              color="grey-lighten-4"
+              max-width="600"
+              v-bind="props"
+            >
+              <v-img
+                :aspect-ratio="16/9"
+                cover
+                src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+              >
+                <v-expand-transition>
+                  <div
+                    v-if="isHovering"
+                    class="d-flex transition-fast-in-fast-out bg-orange-darken-2 v-card--reveal text-h2"
+                    style="height: 100%;"
+                  >
+                    $14.99
+                  </div>
+                </v-expand-transition>
+              </v-img>
+
+              <v-card-text class="pt-6">
+                <div class="font-weight-light text-grey text-h6 mb-2">
+                  For the perfect meal
+                </div>
+
+                <h3 class="text-h4 font-weight-light text-orange mb-2">
+                  QW cooking utensils
+                </h3>
+
+                <div class="font-weight-light text-h6 mb-2">
+                  Our Vintage kitchen utensils delight any chef.<br>
+                  Made of bamboo by hand
+                </div>
+              </v-card-text>
+            </v-card>
+        
+            <!-- <v-card
+              color="basil"
+              flat
+            >
+              <v-card-text>{{ text }}</v-card-text>
+            </v-card> -->
+          </v-hover>
+        </div>
       </v-window-item>
     </v-window>
   </v-card>
 
+  <!--CONTAINER CARDS-->
+
+  <v-card>
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
+
 <!--SCRIPT-->
 <script>
   export default {
@@ -89,7 +171,15 @@
           'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
         ],
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      }
+        cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 4 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 4 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 4 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 4 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 4 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 4 },
+
+      ],}
       }
   }
 </script>
