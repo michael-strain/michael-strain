f71285901fc609f5b738898761069b031468c4fc
ashley-strain <ashley.n.strain@gmail.com>
2023-03-09 17:29:02 -0500

    Account: Logged in front end.

--- pages/account.vue
+++ pages/account.vue
@@ -1,4 +1,16 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+
+<!--To Do
+Edit icon on profile image.
+Flex pic and list.
+Button height.
+Edit Profile, Delete Account, Sign Out links on bottom of gray div.
+Confirmation pop-ups for delete account and sign out.
+Try different images for sizing testing. 
+White card is not floating high enough.
+
+-->
+
 <template>
   <div class="h-screen p-20 <sm:p-5 bg-gradient-to-b from-transparent via-transparent to-gray-600 bg-url('../img/purple-mushroom.jpg')">
     <div>
@@ -33,31 +45,80 @@
         </h6>
       </NuxtLink> -->
       </div>
-      <div v-if="auth.currentUser || firebaseUser">
-        <div class="m-auto bg-white max-w-700px elevation-5 rounded-25px">
-          <div class="p-5 text-lg text-center">
-            <p class="text-gray-600 mt-10">
-              You are currently logged in.
-            </p>
-          </div>          <v-card-subtitle>Someday, actionable content will go here</v-card-subtitle>
-          <v-card-text>I need to add password reset, display name, profile picture upload, account delete, and other settings as they become applicable.</v-card-text>
-          <v-list lines="one">
-            <v-list-item
-              :key="auth.currentUser.uid"
-              :title="auth.currentUser.displayName"
-              :subtitle="auth.currentUser.email"
-              :prepend-avatar="userPhotoUrl"
-              lazy-prepend-avatar="/img/MLogo.png"
-            />
-          </v-list>
+      <div
+        v-if="auth.currentUser || firebaseUser"
+        class="m-auto bg-white max-w-700px elevation-5 rounded-25px"
+      >
+        <!--Image float to the left. Right: User Name, Email, Address-->
+        <div class="flex pt-10 text-lg">
+          <div class="w-1/3 m-3 flex align-center">
+            <img
+              src="/img/MLogo.png"
+            >
+          </div>
+          <div class="flex-grow p-5">
+            <p class="font-bold text-2xl">First Last Name</p>
+            <p>mymessage@email.com</p>
+            <p>258 First Street</p>
+            <p>City, State, 25852</p>
+            <p class="text-gray-500">Edit Profile</p>
+          </div>
+        </div>
+        <!--End Test Code-->
+        <v-card-text>I need to add password reset, display name, profile picture upload, account delete, and other settings as they become applicable.</v-card-text>
+        <!-- <v-list lines="one">
+          <v-list-item
+            :key="auth.currentUser.uid"
+            :title="auth.currentUser.displayName"
+            :subtitle="auth.currentUser.email"
+            :prepend-avatar="userPhotoUrl"
+            lazy-prepend-avatar="/img/MLogo.png"
+          />
+        </v-list> -->
 
-          <v-avatar />
-          <!-- <v-card-text>{{ auth.currentUser }}</v-card-text> -->
-          <v-card-actions>
-            <v-btn @click="signOutUser">
+        <v-avatar />
+        <!-- <v-card-text>{{ auth.currentUser }}</v-card-text> -->
+        <!-- <v-btn @click="signOutUser">
+            Sign Out
+          </v-btn> -->
+        <div
+          class="flex justify-content-around flex-wrap px-5"
+          justify-center
+          align-baseline
+        >
+          <div
+            class="p-2 flex-fill elevation-10 text-white rounded-20px bg-gradient-to-b from-gray-400 to-gray-500 hover:from-yellow-200 hover:via-orange-300 hover:to-red-400"
+          >
+            <v-btn
+              variant="text"
+              class="w-full"
+            >
+              <!-- <v-icon
+                  icon="mdi-google"
+                  color="white"
+                  class="p-4 mr-3"
+                  size="x-large"
+                /> -->
+              Edit Profile
+            </v-btn>
+          </div>
+          <div
+            class="p-2 <md:(mt-6) md:ml-10 text-white flex-fill elevation-10 rounded-20px bg-gradient-to-b from-gray-400 to-gray-500 hover:from-yellow-200 hover:via-orange-300 hover:to-red-400"
+          >
+            <v-btn
+              variant="text"
+              class="w-full"
+              @click="signOutUser"
+            >
+              <!-- <v-icon
+                  icon="mdi-email-outline"
+                  color="white"
+                  class="p-4 mr-3"
+                  size="x-large" 
+                /> -->
               Sign Out
             </v-btn>
-          </v-card-actions>
+          </div>
         </div>
       </div>
       <div
