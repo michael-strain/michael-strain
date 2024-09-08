1a81178fd83f0ba4e5961e1c0f676c7621fa8f1b
Michael Strain <michael.v.strain@gmail.com>
2024-09-08 08:54:56 -0600

    good morning

---
+++ pages/cannabis/index.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  Cannabis Activism, Education, and Entertainment
+
+  -Growth Technique
+  -Plant Variations
+  -My Strain Collection & Reviews
+  -My Cannabis-Based Reality Show
+  -Copyable Letters to Legislators Demanding the Repeal of Cannabis Prohibition, Fair Taxation, and Abolition of Licensure & Grow Permits
+</template>
